95f8fc54a94bb2017de0aee4645a355854937504
TheIns07 <arturoinscreup2401@hotmail.com>
2023-02-16 20:08:11 -0700

    Problema resuelto

--- Main.js
+++ Main.js
@@ -4,28 +4,28 @@ const prompt = require('prompt-sync')();
 
 
 medianaArray = (arrayMain, arraySecondary) => {
-    const concatedArrays = arrayMain.concat(arraySecondary)
-    const lengths = arrayMain.length + arraySecondary.length;
-    const arrayFin = []
 
+    arrayMain = arrayMain.split(",").sort()
+    arraySecondary = arraySecondary.split(",").sort()
+    console.log(arrayMain)
+    console.log(arraySecondary)
 
-    for (let i = 0; i <= lengths; i++) {
-        walkable = parseInt(concatedArrays[i])
-        arrayFin[i] = walkable
-    }
-    
+    let arrayMainSupport = []
+    let arraySecondarySupport = []
 
-    arrayFin.sort((x, y) => x - y)
-    
-    const newArray = arrayFin.filter(function (value) {
-        return !Number.isNaN(value);
-    });
-    console.log(newArray)
+    arrayMain.forEach(e => {
+      arrayMainSupport.push(parseInt(e))
+    })
 
-    let mitad = Math.floor(newArray.length +1 / 2)
+    arraySecondary.forEach(e => {
+        arraySecondarySupport.push(parseInt(e))
+    })
 
+    const concatedArrays = arrayMainSupport.concat(arraySecondarySupport)
+    const lengths = concatedArrays.length;
+    const mitad = concatedArrays.length/2;
 
-    return newArray.length % 2 == 1 ? newArray[mitad] : (newArray[mitad - 1] + newArray[mitad + 2]) / 2
+    return lengths % 2 == 1 ? concatedArrays[mitad] : (concatedArrays[mitad - 1] + concatedArrays[mitad + 2]) / 2
 }
 
 const firstArray = prompt("Introduce los valores del primer arreglo. (Recuerda separar por comas)");
