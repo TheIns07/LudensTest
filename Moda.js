363fabb1ee6c0b403e487865d4ee9e765d1a2c13
TheIns07 <arturoinscreup2401@hotmail.com>
2023-02-17 00:22:19 -0700

    Moda y Bimoda comprobable

--- Moda.js
+++ Moda.js
@@ -1,6 +1,5 @@
 const prompt = require('prompt-sync')();
-
-
+console.clear()
  moda = (listaM) => {
     const lista1Count = {};
     listaM = listaM.split(",").sort()
@@ -10,10 +9,24 @@ const prompt = require('prompt-sync')();
         { lista1Count[elemento] ? lista1Count[elemento] += 1 : lista1Count[elemento] = 1; }
     });
 
-    const lista1Array = Object.entries(lista1Count).sort((a, b) => { return a - b });
+    const lista1Array = Object.entries(lista1Count).sort((a, b) => { return a[1] - b[1] });
     const moda = {};
+    const mainModa = lista1Array[lista1Array.length-1]
+    lista1Array.pop()
 
-    {Object.entries(lista1Array).length > 1 ? Object.assign(moda, {"Bimodal": lista1Array}) : Object.assign(moda, {"Moda": lista1Array[lista1Array-1]})}
+    let bimoda = null;
+    lista1Array.forEach(value => {
+        if(value[1] === mainModa[1]){
+            bimoda = value
+        }
+    })
+    
+    if(!bimoda){
+        Object.assign(moda, {"Moda": mainModa})
+    }
+    else {
+        Object.assign(moda, {"Bimoda": bimoda, mainModa})
+    }
     
     return moda
 }
