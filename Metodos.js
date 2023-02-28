9b2cc4e3bc1c314cea20fbfeceed929004a2cd18
TheIns07 <arturoinscreup2401@hotmail.com>
2023-02-27 23:39:21 -0700

    Arrays: ordenamiento y manipulación

--- Metodos.js
+++ Metodos.js
@@ -1,4 +1,4 @@
-const numbers = [1,2,3,4]
+const numbers = [1,2,3,4,2,2]
 
 //Checar valores del arreglo
 const resultEvery = numbers.every((number, index) => {
@@ -22,8 +22,14 @@ const reduceWithValue = numbers.reduce((valorAnterior, valorAcumulado, index, ar
 console.log(Math.max(...numbers))
 //Solo pares y multiplicar array por 2
 console.log(numbers.filter(x => x%2 ===0).map(x => x * 2))
-//Mutacion de array
-
+//Union de arrays y filtrado de componentes
+console.log(numbers.flatMap(n => n%2===0 ? []: n*2))
+//Ordenamiento random
+console.log(numbers.sort((a,b) => Math.random() - 0.5))
+//Ordenamiento aleatorio
+console.log("Random Item: "+ numbers[Math.floor(Math.random() * numbers.length)])
+//Valores unicos
+Array.from(new Set(numbers))
 
 console.log(reduce)
 console.log(reduceWithValue)
